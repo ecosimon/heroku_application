47f6cd80b18c7b128de25c586674d23f4232fa00
ecosimon <liu_simon_eco@yahoo.com>
2018-06-12 19:00:56 -0400

    settings changed for production

--- website/settings.py
+++ website/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
@@ -78,15 +78,22 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-		'USER': config('DB_USER'),
-		'PASSWORD': config('DB_PASSWORD'),
-		'HOST': config('DB_HOST'),
-		'PORT': config('DB_PORT'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+	)
 }
+
+# DATABASES = {
+    # 'default': {
+        
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': config('DB_NAME'),
+		# 'USER': config('DB_USER'),
+		# 'PASSWORD': config('DB_PASSWORD'),
+		# 'HOST': config('DB_HOST'),
+		# 'PORT': config('DB_PORT'),
+    # }
+# }
 
 
 # Password validation
