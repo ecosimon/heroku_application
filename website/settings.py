4fe17506fa845bcdb5d23d7ae35c75c03c8d2830
ecosimon <liu_simon_eco@yahoo.com>
2018-06-12 20:09:24 -0400

    fixing favicon error

--- website/settings.py
+++ website/settings.py
@@ -130,6 +130,8 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# do not add this in settings as it can cause collect static issues in deployment.
 # STATICFILES_DIRS = (
 	# os.path.join(BASE_DIR, 'static'),
 # )
