4bbe704cf8753dacc47f2ddb3d412884c9fb0419
ecosimon <liu_simon_eco@yahoo.com>
2018-06-13 15:35:14 -0400

    retake

--- website/settings.py
+++ website/settings.py
@@ -155,8 +155,8 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = (
 	# os.path.join(BASE_DIR, 'static'),
 # )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
