f1b598f797ba42a117eef0ee926d254f95c3b270
ecosimon <liu_simon_eco@yahoo.com>
2018-06-13 02:58:20 -0400

    new replacement for staticfile_storage

--- website/settings.py
+++ website/settings.py
@@ -153,4 +153,5 @@ STATIC_URL = '/static/'
 	# os.path.join(BASE_DIR, 'static'),
 # )
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
