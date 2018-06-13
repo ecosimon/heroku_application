27ff64848185c53dd2b070c13c1cf1b99e8decad
ecosimon <liu_simon_eco@yahoo.com>
2018-06-13 00:09:40 -0400

    retake 3

--- website/settings.py
+++ website/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['www.wilsamplesite.herokuapp.com','.wilsamplesite.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['www.wilsamplesite.herokuapp.com','.wilsamplesite.herokuapp.com','.heroku.com','.herokuapp.com']
 
 
 # Application definition
