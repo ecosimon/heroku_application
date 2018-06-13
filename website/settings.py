1e2397d6ba5437d3c26672b6769b0c40128225bb
ecosimon <liu_simon_eco@yahoo.com>
2018-06-13 02:39:40 -0400

    log added

--- website/settings.py
+++ website/settings.py
@@ -115,6 +115,23 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
