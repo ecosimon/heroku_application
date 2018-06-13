8e376543828b255bde0827e6531b3176cb087db8
ecosimon <liu_simon_eco@yahoo.com>
2018-06-13 16:36:39 -0400

    django-heroku added

--- website/settings.py
+++ website/settings.py
@@ -161,4 +161,4 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-
+django_heroku.settings(locals())
